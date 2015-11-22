9ce5dd802f5af928b49685ce4d5d1ff93bba2c20
railsbeginner <bartosz.dyja@gmail.com>
2015-11-22 16:53:55 +0100

    Create accounts whilst student creation

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,9 +2,15 @@ class Student < ActiveRecord::Base
   has_many :participations, dependent: :destroy
   has_many :subject_item_notes, dependent: :destroy
   has_many :subject_items, through: :participations
-  has_one :account
+  has_one :account, dependent: :destroy
 
   accepts_nested_attributes_for :subject_items
 
+  after_create :set_payment_date
+
   validates :first_name, :last_name, presence: true
+
+  def set_payment_date
+    Account.create!(payment_date: Date.today + 30.days, student: self)
+  end
 end
